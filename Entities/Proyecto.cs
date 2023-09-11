e74d2ae7b1cbfc8459e2f6c68a50c0d1b7edc2e8
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-09-11 20:55:44 -0300

    creacion entities

---
+++ Entities/Proyecto.cs
@@ -0,0 +1,11 @@
+﻿namespace TrabajoIntegradorSofttek.Entities
+{
+    public class Proyecto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int Estado { get; set; }
+        bool Activo { get; set; }
+    }
+}
---
+++ Entities/Trabajo.cs
@@ -0,0 +1,15 @@
+﻿namespace TrabajoIntegradorSofttek.Entities
+{
+    public class Trabajo
+    {
+
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int IdProyecto { get; set; }
+        public int IdServicio { get; set; }
+        public int CantHoras { get; set; }
+        public decimal ValorHora { get; set; }
+        public decimal Costo { get; set; }
+        bool Activo { get; set; }
+    }
+}
