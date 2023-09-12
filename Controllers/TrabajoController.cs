40ce5ba18fd18f4f1100fe441b6fb7829cb278a1
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-09-11 21:20:48 -0300

    creacion controllers y dtos

---
+++ Controllers/TrabajoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrabajoIntegradorSofttek.Entities;
+using TrabajoIntegradorSofttek.DTOs;
+
+namespace TrabajoIntegradorSofttek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrabajoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Trabajos")]
+        public IActionResult Trabajos()
+        {
+            return Ok("Todos los trabajos");
+        }
+
+
+        [HttpPost]
+        [Route("Agregar")]
+        public IActionResult AgregarTrabajo(TrabajoDto trabajo)
+        {
+            return Unauthorized("No se pudo agregar trabajo");
+        }
+
+
+        [HttpPut]
+        [Route("Editar")]
+        public IActionResult EditarTrabajo(TrabajoDto trabajo)
+        {
+            return Ok(trabajo);
+        }
+
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public IActionResult EliminarTrabajo(int id)
+        {
+            return Ok($"Se elimino el elemento {id} correctamente");
+        }
+    }
+}
