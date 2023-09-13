799577c9c274d0c67fd258e007f9e63861d2bfe2
FrancoScaglione203 <franco.scaglione2@gmail.com>
2023-09-12 22:24:57 -0300

    config program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 namespace TrabajoIntegradorSofttek
 {
@@ -7,17 +12,50 @@ namespace TrabajoIntegradorSofttek
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             Log.Logger = new LoggerConfiguration().Enrich.FromLogContext().ReadFrom.Configuration(builder.Configuration).CreateLogger();
             builder.Host.UseSerilog(Log.Logger);
-
             // Add services to the container.
-            builder.Services.AddAuthorization();
 
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Autorizacion JWT",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
 
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type= ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    }, new string[]{ }
+                    }
+                });
+
+            });
+
+
+
+
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            });
 
 
             var app = builder.Build();
@@ -31,8 +69,11 @@ namespace TrabajoIntegradorSofttek
 
             app.UseHttpsRedirection();
 
-           // app.UseAuthentication();
+            app.UseAuthentication();
             app.UseAuthorization();
+
+
+            app.MapControllers();
 
             app.Run();
         }
